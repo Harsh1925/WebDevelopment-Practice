46e43c212a56a17c3aed8df0fc721d91e8c63540
Harsh Modi <d007modi@gmail.com>
2023-02-05 19:26:55 +0630

    Updated Code

--- _Mongoose_Express/index.js
+++ _Mongoose_Express/index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
 const Product = require("./models/product")
+const methodOverride = require("method-override");
+const { triggerAsyncId } = require("async_hooks");
 
 mongoose.connect('mongodb://127.0.0.1:27017/shopApp')
     .then(() => {
@@ -15,9 +18,46 @@ mongoose.connect('mongodb://127.0.0.1:27017/shopApp')
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
 
 app.get("/dog", (req, res) => {
     res.send("Woof!!!")
+})
+
+app.get("/products", async (req, res) => {
+    const products = await Product.find({})
+    res.render("products/index", { products })
+})
+
+
+app.get("/products/new", (req, res) => {
+    res.render("products/new")
+})
+
+app.post("/products", async (req, res) => {
+    const newProduct = new Product(req.body)
+    await newProduct.save()
+    res.redirect(`/products/${newProduct.id}`)
+
+})
+
+app.get("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    res.render("products/show", { product })
+})
+
+app.get("/products/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    res.render("products/edit", { product })
+})
+
+app.put("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+    res.redirect(`/products/${product._id}`);
 })
 
 app.listen(3000, () => {
